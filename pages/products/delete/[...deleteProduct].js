e6bdce89638b8d771b3a39199fe1e6776617f3a3
mahad002 <mahad112002@gmail.com>
2023-12-26 19:49:22 +0500

    Updated store page with products view as well

--- pages/products/delete/[...deleteProduct].js
+++ pages/products/delete/[...deleteProduct].js
@@ -34,7 +34,7 @@ export default function DeleteProductPage() {
     return (
 
         <Layout>
-            <h1 className="text-center">Do you really want to delete &nbsp;"{productInfo?.title}" ?</h1>
+            <h1 className="text-center">Do you really want to delete &nbsp;&quot;{productInfo?.title}&quot; ?</h1>
             <div className="flex gap-2 justify-center">
                 <button onClick={deleteProduct1} className="btn btn-alert bg-red-900 rounded-md m-1 px-3 text-white">Yes</button>
                 <button onClick={goBack} className="btn btn-primary m-1">No</button>
