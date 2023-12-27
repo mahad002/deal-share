aebca57782ea96e0e91733993ec20bdd93315144
“Jakob <jnunnendorf@icloud.com>
2023-12-27 14:18:22 +0800

    fix(next.config.js & package.json): disable webpack modules and add net
    There are server side modules that aren't available on browser. Disabled them in browser environment in next config to see if they deploy now. Also added net module to package.json

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = {nextConfig}
+module.exports = {
+    reactStrictMode: true,
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            config.resolve.fallback = {
+                dns: false,
+                fs: false,
+                tls: false,
+            };
+        }
+
+        return config;
+    },
+};
