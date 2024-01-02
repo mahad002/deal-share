c97c740eda1d57032aafa8067b7e075bb6e459cc
mahad002 <mahad112002@gmail.com>
2024-01-02 09:34:49 +0500

    commented api-doc

--- pages/api-doc.ts
+++ pages/api-doc.ts
@@ -1,32 +1,32 @@
-import { GetStaticProps, InferGetStaticPropsType } from 'next';
-import { createSwaggerSpec } from 'next-swagger-doc';
-import dynamic from 'next/dynamic';
-import 'swagger-ui-react/swagger-ui.css';
+// import { GetStaticProps, InferGetStaticPropsType } from 'next';
+// import { createSwaggerSpec } from 'next-swagger-doc';
+// import dynamic from 'next/dynamic';
+// import 'swagger-ui-react/swagger-ui.css';
 
-const SwaggerUI = dynamic<{
-  spec: any;
-}>(import('swagger-ui-react'), { ssr: false });
+// const SwaggerUI = dynamic<{
+//   spec: any;
+// }>(import('swagger-ui-react'), { ssr: false });
 
-function ApiDoc({ spec }: InferGetStaticPropsType<typeof getStaticProps>) {
-  return <SwaggerUI spec={spec} />;
-}
+// function ApiDoc({ spec }: InferGetStaticPropsType<typeof getStaticProps>) {
+//   return <SwaggerUI spec={spec} />;
+// }
 
-export const getStaticProps: GetStaticProps = async () => {
-  const spec: Record<string, any> = createSwaggerSpec({
-    definition: {
-      openapi: '3.0.0',
-      info: {
-        title: 'Next Swagger API Example',
-        version: '1.0',
-      },
-    },
-  });
+// export const getStaticProps: GetStaticProps = async () => {
+//   const spec: Record<string, any> = createSwaggerSpec({
+//     definition: {
+//       openapi: '3.0.0',
+//       info: {
+//         title: 'Next Swagger API Example',
+//         version: '1.0',
+//       },
+//     },
+//   });
 
-  return {
-    props: {
-      spec,
-    },
-  };
-};
+//   return {
+//     props: {
+//       spec,
+//     },
+//   };
+// };
 
-export default ApiDoc;
+// export default ApiDoc;
